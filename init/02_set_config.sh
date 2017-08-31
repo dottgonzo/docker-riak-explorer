115fc43708ac8fcc78f84207b5f29dbde877fa6b
dario <darioyzf@gmail.com>
2017-08-31 17:25:18 +0200

    up

--- init/02_set_config.sh
+++ init/02_set_config.sh
@@ -6,7 +6,7 @@ RIAK_EXPLORER_PORT=${RIAK_EXPLORER_PORT:-9000}
 RIAK_EXPLORER_NODE_NAME=${RIAK_EXPLORER_NODE_NAME:-riak_explorer}
 RIAK_IFACE=${RIAK_IFACE:-eth0}
 
-IP_ADDRESS=$(ip -o -4 addr list $RIAK_IFACE | awk '{print $4}' | cut -d/ -f1)
+IP_ADDRESS=$(ip -o -4 addr list $RIAK_IFACE | awk '{print $4}' | cut -d/ -f1 2> /dev/null)
 RIAK_NODE_NAME=${RIAK_NODE_NAME:-riak_member}
 RIAK_NODE=${RIAK_NODE:-$RIAK_NODE_NAME@$IP_ADDRESS}
 
